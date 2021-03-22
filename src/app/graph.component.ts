b5f2dbe7b96a770531e406183ab5540849a2ef4a
Ashley <ashley.nguyen27@berkeley.edu>
2021-03-21 18:22:29 -0700

    graph in /app

---
+++ src/app/graph.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {states} from './states';
+
+@Component({
+  selector: 'directed-graph',
+  templateUrl: './graph.component.html',
+  styleUrls: ['./graph.component.scss']
+})
+export class GraphComponent implements OnInit {
+
+  constructor() { 
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+}
